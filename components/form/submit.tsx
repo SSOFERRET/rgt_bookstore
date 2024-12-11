fd606f1f94b9f2a9422eb04daf25bf5c95b21a9b
ssoferret <owosso1212@gmail.com>
2024-12-11 23:15:10 +0900

    Design: 폼 제출 버튼 디자인 수정

--- components/form/submit.tsx
+++ components/form/submit.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { useFormStatus } from "react-dom";
+import Button from "../common/button";
 
 export default function Submit() {
   const { pending } = useFormStatus();
 
-  return <button>
+  return <Button>
     {pending ? '제출 중...' : '제출'}
-  </button>
+  </Button>
 }
