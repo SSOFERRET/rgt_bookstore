d9f60dc7a7ff729760ae04d51aab1d7a2f1359ac
ssoferret <owosso1212@gmail.com>
2024-12-11 22:19:39 +0900

    Refact: 책 전체 조회 페이지 /book url 아래로 이동

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,7 +1,8 @@
-import styles from "./page.module.css";
 import BooksList from "@/components/books/books-list";
 import PaginationBar from "@/components/pagination/pagination-bar";
 import { getBooks } from "@/lib/books";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -14,9 +15,10 @@ export default function Home({ searchParams }: { searchParams: { page?: string }
   const { data, total } = getBooks(page);
 
   return (
-    <div className={styles.page}>
-      <header>
+    <div>
+      <header className={styles.header}>
         <PaginationBar total={total} />
+        <Link className={styles.postButton} href='/book/post'>책 추가</Link>
       </header>
       <main>
         <BooksList books={data} />
