a0558bc0270829235289247c394dd5c2f20cc4ae
ssoferret <owosso1212@gmail.com>
2024-12-11 23:15:36 +0900

    Refact: 검색바에 검색어가 계속 남아있는 문제 수정

--- components/header/search-bar.tsx
+++ components/header/search-bar.tsx
@@ -3,29 +3,35 @@
 import { useState, useEffect } from "react";
 import styles from "./search-bar.module.css";
 import { MdSearch } from "react-icons/md";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter, usePathname } from "next/navigation";
 
 export default function SearchBar() {
   const [search, setSearch] = useState<string>(""); 
   const [debouncedSearch, setDebouncedSearch] = useState<string>("");
   const router = useRouter();
+  const pathname = usePathname();
   const params = useParams();
   const currentQuery = params?.query || "";
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedSearch(search);
-    }, 300);
+    }, 100);
 
     return () => clearTimeout(timer);
   }, [search]);
+
+  useEffect(() => {
+    if (pathname !== "/book/search") {
+      setSearch("");
+    }
+  }, [pathname]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const onSearch = () => {
-    console.log(currentQuery)
     if (debouncedSearch.trim() && debouncedSearch.trim() !== (currentQuery as string).trim()) {
       router.push(`/book/search?query=${debouncedSearch}`);
     }
