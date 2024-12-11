1bbf7bbfaef0048c3639b8dd8af8f3f230638347
ssoferret <owosso1212@gmail.com>
2024-12-11 19:13:28 +0900

    Feat: 데이터 저장 함수 saveBook 작성 - Submit action 작성 - 입력 데이터 validate - 제출 데이터 타입 인터페이스

--- lib/books.ts
+++ lib/books.ts
@@ -1,6 +1,9 @@
 import { initializeDatabase } from "@/initdb";
-import { IBook } from "@/types/book.type";
+import { IBook, ISaveBook } from "@/types/book.type";
 import sql from 'better-sqlite3';
+import xss from "xss";
+import fs from 'node:fs';
+
 const db = sql(`books.db`);
 
 export function getBooks(page: number = 1, limit: number = 10): { data: IBook[]; total: number } {
@@ -22,6 +25,57 @@ export function getBook(id: number): IBook {
   return db.prepare('SELECT * FROM books WHERE id = ?').get(id) as IBook;
 }
 
+export async function saveBook(book: ISaveBook) {
+  book.description = xss(book.description);
+
+  const extension = book.image.name.split('.').pop();
+  const fileName = `image_${Date.now()}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await book.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed.');
+    }
+  });
+
+  const newBook: IBook = {
+    ...book,
+    image: `/images/${fileName}`
+  };
+
+  const result = db.prepare(`
+    INSERT INTO books 
+      (
+        title,
+        image,
+        author,
+        publicationDate,
+        page,
+        isbn,
+        description,
+        publisher,
+        currentStock,
+        price
+      ) VALUES 
+      (
+        @title,
+        @image,
+        @author,
+        @publicationDate,
+        @page,
+        @isbn,
+        @description,
+        @publisher,
+        @currentStock,
+        @price
+      )
+  `).run(newBook);
+
+  return result.lastInsertRowid;
+}
+
 function executeWithRetry<T>(task: () => T): T {
   try {
     return task();
@@ -31,3 +85,4 @@ function executeWithRetry<T>(task: () => T): T {
     return task();
   }
 }
+
--- types/book.type.ts
+++ types/book.type.ts
@@ -13,4 +13,19 @@ export interface IBook {
   page?: number;
 }
 
+export interface ISaveBook {
+  id: number;
+  title: string;
+  image: File;
+  author: string;
+  publicationDate: string;
+  isbn: string;
+  description: string;
+  publisher: string;
+  currentStock: number;
+  totalSales: number;
+  price: number;
+  page?: number;
+}
+
 export const PAGE_LIMIT = 10;
