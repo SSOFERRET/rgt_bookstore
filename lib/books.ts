c29b2c64d9f2a91930685dff60a1a4f5d6190c5a
ssoferret <owosso1212@gmail.com>
2024-12-11 13:07:06 +0900

    Feat: 페이지네이션 기능 위해 sql 카운트 구문 추가

--- lib/books.ts
+++ lib/books.ts
@@ -3,11 +3,22 @@ import { IBook } from "@/types/book.type";
 import sql from 'better-sqlite3';
 const db = sql(`books.db`);
 
-export function getBooks(): IBook[] {
+export function getBooks(page: number = 1, limit: number = 10): { data: IBook[], total: number } {
+  const offset = (page - 1) * limit;
+
   try {
-    return db.prepare('SELECT * FROM books').all() as IBook[];
-  } catch {
+    const data = db.prepare('SELECT * FROM books LIMIT ? OFFSET ?').all(limit, offset) as IBook[];
+
+    const result = db.prepare('SELECT COUNT(*) AS count FROM books').get() as { count: number };
+    const total = result.count;
+
+    return { data, total };
+  } catch (error) {
     initializeDatabase();
-    return db.prepare('SELECT * FROM books').all() as IBook[];
+    const result = db.prepare('SELECT COUNT(*) AS count FROM books').get() as { count: number };
+    const total = result.count;
+
+    const data = db.prepare('SELECT * FROM books LIMIT ? OFFSET ?').all(limit, offset) as IBook[];
+    return { data, total };
   }
 }
